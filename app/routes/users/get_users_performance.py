ad229093da489120e0a20be7a45be950264c06ee
myseldesai <amardesai.bgm@gmail.com>
2022-08-04 16:37:32 +0100

    Task 1

--- app/routes/users/get_users_performance.py
+++ app/routes/users/get_users_performance.py
@@ -1,9 +1,10 @@
-from app import app
+from sqlalchemy import text
+from app import app, database
 from flask import jsonify
 
 
 # TODO: Give the route a name/path.
-@app.route("/your-route-name-here", methods=["GET"])
+@app.route("/users/stats", methods=["GET"])
 def get_users_performance():
     """
     Return the interactions completed and average handling time of every user.
@@ -23,8 +24,12 @@ def get_users_performance():
 
     # TODO: Calculate the statistics for each agent.
 
-    return jsonify([
-        {"userId": 1, "name": "Test User 1", "chatsHandled": 10, "averageHandlingSeconds": 120},
-        {"userId": 2, "name": "Test User 2", "chatsHandled": 0, "averageHandlingSeconds": 0},
-        # etc.
-    ])
+
+    query = text('SELECT user.user_id, user.name, COUNT(chat.user_id), AVG(strftime("%s",chat.handle_end)-strftime("%s",chat.handle_start)) FROM user LEFT JOIN chat ON chat.user_id = user.user_id GROUP BY chat.user_id')
+    result = database.session.execute(query)
+    payload = []
+  
+    for res in result.fetchall() :
+        payload.append({"userId": res[0], "name": res[1], "chatsHandled": res[2], "averageHandlingSeconds": int(res[3] or 0)})
+
+    return jsonify(payload)
