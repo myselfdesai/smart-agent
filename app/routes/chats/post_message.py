86266dbe8e43f7084607b916b75ba2ac20cf7472
myseldesai <amardesai.bgm@gmail.com>
2022-08-04 16:44:01 +0100

    Task 2

--- app/routes/chats/post_message.py
+++ app/routes/chats/post_message.py
@@ -1,11 +1,10 @@
-from http import HTTPStatus
-from typing import Tuple
 import re
 
 from app import app, database
-from flask import request
-
 from app.models import Chat, Message
+from flask import request
+from http import HTTPStatus
+from typing import Tuple
 
 
 @app.route("/chats/<int:chat_id>/messages", methods=["POST"])
@@ -25,57 +24,58 @@ def post_message(chat_id: int) -> Tuple[str, int]:
 
     chat: Chat = Chat.query.get_or_404(chat_id)
 
-    if _strip_sensitive_text(request.json.get("text")):
-        return "Credit card numbers are not allowed.", HTTPStatus.BAD_REQUEST
+    message = request.json.get("text")
+    user_id = request.json.get("user_id")
 
-    # Store the message.
-    message = Message(chat=chat, text=request.json.get("text"))
-    database.session.add(message)
-    database.session.commit()
-
-    if chat.handle_start is None:
+    if message is None:
+        return "Please provide valid parameter ex. {'text': 'some message'}", HTTPStatus.BAD_REQUEST
+    elif chat.handle_start is None:
         return "Can't store a message before the chat has started.", HTTPStatus.BAD_REQUEST
-
-    if chat.handle_end is None:
+    elif chat.handle_end is not None:
         return "Can't store a message after the chat has ended.", HTTPStatus.BAD_REQUEST
-
-    if not _is_handling_agent(chat, message):
+    elif user_id is not None and not _is_handling_agent(chat, user_id):
         return "That agent isn't handling the chat.", HTTPStatus.BAD_REQUEST
+    elif _is_too_long(message):
+        return "The message cannot be more than 500 characters.", HTTPStatus.BAD_REQUEST
+    else:
+        # Store the message.
+        message = _strip_sensitive_text(message)
 
-    if _is_too_long(chat):
-        return "The message cannot be more than 400 characters.", HTTPStatus.BAD_REQUEST
+        message = Message(chat_id=chat.chat_id, text=message, user_id=chat.user_id)
+        database.session.add(message)
+        database.session.commit()
 
-    return "", HTTPStatus.CREATED
+        return "stored", HTTPStatus.CREATED
 
 
-def _is_handling_agent(chat: Chat, message: Message) -> bool:
+def _is_handling_agent(chat: Chat, user_id: int) -> bool:
     """
     If the message has a user ID, it has to match the agent of the chat.
     :param chat:    Chat being updated.
-    :param message: Message being stored.
     :return:        True if the agent is correct or False otherwise.
     """
-    if request.json.get("user_id") != chat.user_id:
-        database.session.delete(message)
-        database.session.commit()
+    if user_id != chat.user_id:
         return False
-
     return True
 
 
-def _is_too_long(chat: Chat) -> bool:
+def _is_too_long(message: str) -> bool:
     """
     Don't store messages that are too long.
     :param chat: Chat being updated.
     :return:     True if the message is too long.
     """
-    message: Message = chat.messages[0]
-    return len(message.text) >= 500
+    if len(message) <= 500:
+        return False
+    return True
 
 
-def _strip_sensitive_text(text: str) -> bool:
+def _strip_sensitive_text(message: str) -> bool:
     """
     Remove credit card numbers.
     :param text: Message text.
+    only handled visa, mastercard and american express for this project
     """
-    return re.search(r"\d{4} \d{4} \d{4} \d{4}", text) is not None
+
+    message = re.sub('4[0-9]{12}(?:[0-9]{3})|(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}|3[47][0-9]{13}', '*******', message)
+    return message
